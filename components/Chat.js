a2844710c9ca655a89945c16e8296056a1eeb15c
Kittekat14 <katihofstetter@gmail.com>
2021-11-05 10:28:38 +0100

    state "messages" and setMessages when component did mount

--- components/Chat.js
+++ components/Chat.js
@@ -2,6 +2,31 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 export default class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+      ],
+    });
+  }
+
   render() {
     let text = this.props.route.params.text;
     let color = this.props.route.params.color;
