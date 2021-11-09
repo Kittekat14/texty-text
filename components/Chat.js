30069a1253b3852428c48bca79b75403b87cbdd4
Kittekat14 <katihofstetter@gmail.com>
2021-11-09 12:27:31 +0100

    Chat.js back to last state of yesterday

--- components/Chat.js
+++ components/Chat.js
@@ -1,9 +1,9 @@
 import React from "react";
 import firebase from "firebase";
-import firestore from "firebase";
+import "firebase/firestore";
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,23 +11,23 @@ import { KeyboardAvoidingView, View, Platform } from "react-native";
 import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 // Your web app's Firebase configuration
-    const firebaseConfig = {
-      apiKey: "AIzaSyAVl8wJ9Xy9PkAQjvY72ah1JD6ISfuT_bA",
-      authDomain: "lessenger-d5b80.firebaseapp.com",
-      projectId: "lessenger-d5b80",
-      storageBucket: "lessenger-d5b80.appspot.com",
-      messagingSenderId: "475201686420",
-      appId: "1:475201686420:web:86ff783a4735c6ec46dd81",
-    };
+const firebaseConfig = {
+  apiKey: "AIzaSyAVl8wJ9Xy9PkAQjvY72ah1JD6ISfuT_bA",
+  authDomain: "lessenger-d5b80.firebaseapp.com",
+  projectId: "lessenger-d5b80",
+  storageBucket: "lessenger-d5b80.appspot.com",
+  messagingSenderId: "475201686420",
+  appId: "1:475201686420:web:86ff783a4735c6ec46dd81",
+};
 
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      uid: "",
+      uid: 0,
       messages: [],
       user: {
-        _id: 0,
+        _id: "",
         name: "",
         avatar: "",
       },
@@ -47,14 +47,13 @@ export default class Chat extends React.Component {
 
     this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
       if (!user) {
-        //must not be a user to be signed in (anonymously)
         await firebase.auth().signInAnonymously();
       }
 
       this.setState({
-        uid: this.state.uid,
+        uid: user.uid,
         user: {
-          _id: 3,
+          _id: user.uid,
           name: text,
           avatar: "https://placeimg.com/140/140/any",
         },
@@ -63,7 +62,7 @@ export default class Chat extends React.Component {
 
       // reference to messages collection
       this.unsubscribeChatUser = this.referenceMessages
-        .orderBy("createdAt", "desc") //desc = descendent
+        .orderBy("createdAt", "desc")
         .onSnapshot(this.onCollectionUpdate);
     });
   }
@@ -74,20 +73,20 @@ export default class Chat extends React.Component {
     this.unsubscribeChatUser();
   }
 
-  onCollectionUpdate = async (querySnapshot) => {
+  onCollectionUpdate = (querySnapshot) => {
     const messages = [];
     // go through each document
-    await querySnapshot.forEach((doc) => {
+    querySnapshot.forEach((doc) => {
       // get the querysnapshot's data
       let data = doc.data();
       messages.push({
-        uid: data.uid,
+        _id: data._id,
         text: data.text,
         createdAt: data.createdAt.toDate(),
         user: data.user,
       });
     });
-    await this.setState({
+    this.setState({
       messages,
     });
   };
@@ -104,11 +103,13 @@ export default class Chat extends React.Component {
   }
 
   onSend(messages = []) {
-    this.addMessage();
     this.setState(
       (previousState) => ({
         messages: GiftedChat.append(previousState.messages, messages),
-      })
+      }),
+      () => {
+        this.addMessage();
+      }
     );
   }
 
@@ -138,14 +139,13 @@ export default class Chat extends React.Component {
     return (
       <View style={{ flex: 1, backgroundColor: color, color: "#000" }}>
         <GiftedChat
-          key={this.state.uid}
           renderBubble={this.renderBubble.bind(this)}
           messages={this.state.messages}
           onSend={(messages) => this.onSend(messages)}
           user={{
-            _id: this.state.user._id,
-            name: this.state.user.name,
-            avatar: this.state.user.avatar,
+            _id: this.state.uid,
+            name: this.props.route.params.text,
+            avatar: "https://placeimg.com/140/140/any",
           }}
         />
 
