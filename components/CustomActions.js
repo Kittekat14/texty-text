382e7687867e1390f885eb1d78eb7fa1b246d965
Kittekat14 <katihofstetter@gmail.com>
2021-11-15 17:50:23 +0100

    try-catch block in 3 async functions and reference to each inside onActionPress

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -16,6 +16,72 @@ export default class CustomActions extends Component {
     };
   }
 
+  // Let the user pick an image from the device's image library:
+  pickImage = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    try {
+      if (status === "granted") {
+        const result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images, // only images are allowed
+        }).catch((error) => console.log(error));
+        if (!result.cancelled) {
+          const imageUrl = await this.uploadImageFetch(result.uri);
+          this.props.onSend({ image: imageUrl });
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  // Let the user take a photo with device's camera:
+  takePhoto = async () => {
+    const { status } = await Permissions.askAsync(
+      Permissions.CAMERA,
+      Permissions.CAMERA_ROLL
+    );
+    try {
+      if (status === "granted") {
+        const result = await ImagePicker.launchCameraAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        }).catch((error) => console.log(error));
+
+        if (!result.cancelled) {
+          const imageUrl = await this.uploadImageFetch(result.uri);
+          this.props.onSend({ image: imageUrl });
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  //get the location of the user by using GPS:
+  getLocation = async () => {
+    try {
+      const { status } = await Permissions.askAsync(Permissions.LOCATION);
+      if (status === "granted") {
+        const result = await Location.getCurrentPositionAsync({}).catch(
+          (error) => console.log(error)
+        );
+        const longitude = JSON.stringify(result.coords.longitude);
+        const altitude = JSON.stringify(result.coords.latitude);
+        if (result) {
+          this.props.onSend({
+            location: {
+              longitude: result.coords.longitude,
+              latitude: result.coords.latitude,
+            },
+          });
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  //Upload images to firebase:
+
   uploadImageFetch = async (uri) => {
     const blob = await new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
@@ -33,14 +99,20 @@ export default class CustomActions extends Component {
 
     const imageNameBefore = uri.split("/");
     const imageName = imageNameBefore[imageNameBefore.length - 1];
+
     const ref = firebase.storage().ref().child(`images/${imageName}`);
+
     const snapshot = await ref.put(blob);
+
     blob.close();
 
     return await snapshot.ref.getDownloadURL();
   };
-  
 
+  /**
+   * function that handles communication features
+   * @function onActionPress
+   */
   onActionPress = () => {
     const options = [
       "Choose From Library",
@@ -58,60 +130,13 @@ export default class CustomActions extends Component {
         switch (buttonIndex) {
           case 0:
             console.log("user wants to pick an image");
-            // pickImage = async () => {
-            //   const { status } = await Permissions.askAsync(
-            //     Permissions.CAMERA_ROLL
-            //   );
-
-            //   if (status === "granted") {
-            //     let result = await ImagePicker.launchImageLibraryAsync({
-            //       mediaTypes: "Images",
-            //     }).catch((error) => console.log(error));
-
-            //     if (!result.cancelled) {
-            //       setState({
-            //         image: result,
-            //       });
-            //     }
-            //   }
-            // };
-            return;
+            return this.imagePicker();
           case 1:
             console.log("user wants to take a photo");
-            // takePhoto = async () => {
-            //   const { status } = await Permissions.askAsync(
-            //     Permissions.CAMERA_ROLL,
-            //     Permissions.CAMERA
-            //   );
-            //   if (status === "granted") {
-            //     let result = await ImagePicker.launchCameraAsync().catch(
-            //       (error) => console.log(error)
-            //     );
-            //     if (!result.cancelled) {
-            //       setState({
-            //         image: result,
-            //       });
-            //     }
-            //   }
-            // };
-            return;
+            return this.takePhoto();
           case 2:
             console.log("user wants to get their location");
-            // getLocation = async () => {
-            //   const { status } = await Permissions.askAsync(
-            //     Permissions.LOCATION
-            //   );
-            //   if (status === "granted") {
-            //     let result = await Location.getCurrentPositionAsync({});
-            //     if (result) {
-            //       setState({
-            //         location: result,
-            //       });
-            //     }
-            //   }
-            // };
-          default:
-            console.log("this is default case");
+            return this.getLocation();
         }
       }
     );
